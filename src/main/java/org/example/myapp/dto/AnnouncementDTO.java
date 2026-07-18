892f018bf52f0dd4684db1cbbe94c577ac1a6374
Robert Ligeza <robertligeza2@gmail.com>
2026-07-18 14:40:20 +0200

    Declare public List<@Valid AnnouncementTranslationDTO> translations;

--- src/main/java/org/example/myapp/dto/AnnouncementDTO.java
+++ src/main/java/org/example/myapp/dto/AnnouncementDTO.java
@@ -35,17 +35,14 @@ public class AnnouncementDTO {
     @NotNull(message = "Reception date/time is required")
     public LocalDateTime receptionDateTime;
 
-    @Valid
     @Size(min = 1, message = "At least one translation is required")
-    public List<AnnouncementTranslationDTO> translations;
+    public List<@Valid AnnouncementTranslationDTO> translations;
 
-    @Valid
     @Size(min = 0, message = "Stops list cannot be null")
-    public List<StopDTO> stops;
+    public List<@Valid StopDTO> stops;
 
-    @Valid
     @Size(min = 0, message = "Photos list cannot be null")
-    public List<PhotoDTO> photos;
+    public List<@Valid PhotoDTO> photos;
 }
 
 
